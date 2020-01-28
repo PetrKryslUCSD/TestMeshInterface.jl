670da1ac4bccb5b3651c5fefa9c15906c53578f5
Petr Krysl <pkrysl@ucsd.edu>
2020-01-27 17:14:48 -0800

    More tests

--- src/TestMeshInterface.jl
+++ src/TestMeshInterface.jl
@@ -2,7 +2,7 @@ module TestMeshInterface
 
 using MeshInterface: NoSuchTrait, FESetTrait, FEMeshTrait
 import MeshInterface: fesettrait, femeshtrait
-import MeshInterface: _numberofelements, _getconn!
+import MeshInterface: _numberofsets, _numberofelements, _getconn!, _numberofnodesperelement
 using FinEtools
 
 struct FEMesh
@@ -13,6 +13,8 @@ end
 fesettrait(::Type{T}) where {T<:FinEtools.FESetModule.AbstractFESet} = FESetTrait()
 femeshtrait(::Type{FEMesh}) = FEMeshTrait()
 
+_numberofsets(::FEMeshTrait, m::FEMesh) = length(m.fesets)
+_numberofnodesperelement(::FESetTrait, fes::T) where {T} = nodesperelem(fes)
 _numberofelements(::FESetTrait, fes::T) where {T} = length(fes.conn)
 _getconn!(::FESetTrait, c::C, fes::T, i)  where {C, T} = (c .= fes.conn[i])
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 module m1a
 using FinEtools
-using MeshInterface: numberofelements, getconn!
+using MeshInterface: numberofelements, getconn!, numberofsets, numberofnodesperelement
 using TestMeshInterface: FEMesh
 using Test
 function test()
@@ -9,8 +9,9 @@ function test()
     fens,fes = T3block(A, A, N, N)
     mesh = FEMesh(fens, [fes])
 
+    @test numberofsets(mesh) == 1
     @show numberofelements(mesh.fesets[1])
-    c = fill(0, 3)
+    c = fill(0, numberofnodesperelement(mesh.fesets[1]))
     @show getconn!(c, mesh.fesets[1], 1)
     @test c[1] == fes.conn[1][1]
     @test c[3] == fes.conn[1][3]
